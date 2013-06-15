d27721684750e6c2beb1ff4d3e2adba8f0ed57fa
kapil <kchoudhary006@gmail.com>
2013-06-15 17:56:52 +0530

    static part home page done

---
+++ app/assets/javascripts/bottom_section.js
@@ -0,0 +1,10 @@
+//= require jquery-ui-1.10.1.custom.min
+//= require bootstrap.min
+//= require breakpoints
+//= require jquery.slimscroll.min
+//= require jquery.blockui
+//= require jquery.cookie
+//= require jquery.uniform.min
+//= require app
+
+
