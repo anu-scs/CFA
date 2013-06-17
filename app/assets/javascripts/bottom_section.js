89b6960002b73559a61f587c184a5dbc7758c6a8
kapil <kchoudhary006@gmail.com>
2013-06-17 07:22:22 +0530

    static page capmaign detail

--- app/assets/javascripts/bottom_section.js
+++ app/assets/javascripts/bottom_section.js
@@ -6,5 +6,22 @@
 //= require jquery.cookie
 //= require jquery.uniform.min
 //= require app
+//= require table-editable
+//= require table-managed
+//= require ui-sliders
+//= require ui-general
+//= require jquery.ui.touch-punch.min
+// <!--[if lt IE 9]>
+//= require excanvas
+//= require respond
+// <![endif]-->   
+//= require jquery.knob
+//= require select2.min
+//= require jquery.dataTables
+//= require DT_bootstrap
+//= require jquery.gritter
+//= require jquery.pulsate.min
 
 
+
+// require jquery.ui.slider-rtl.min
