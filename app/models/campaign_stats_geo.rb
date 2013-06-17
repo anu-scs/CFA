4fd004d98f20b8afbf992256d8d51e6ce710829a
kapil <kchoudhary006@gmail.com>
2013-06-18 02:00:11 +0530

    break down compaign index and make association

--- app/models/campaign_stats_geo.rb
+++ app/models/campaign_stats_geo.rb
@@ -1,4 +1,5 @@
-class Geocamp < ActiveRecord::Base
+class CampaignStatsGeo < ActiveRecord::Base
+  self.table_name = "campaign_stats_geo"
   belongs_to :campaign
   
   attr_accessible :states_called_now, :states_called_today, :states_included_all, :zipcodes_called_now, 
