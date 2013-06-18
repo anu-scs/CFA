f9021efbd83b0629e2280f54912c3e26adb27760
kapil <kchoudhary006@gmail.com>
2013-06-19 00:42:45 +0530

    change in config

--- config/application.rb
+++ config/application.rb
@@ -57,7 +57,7 @@ module Cfa
     config.assets.enabled = true
     
     config.assets.initialize_on_precompile = false
-    config.assets.precompile += ['bottom_section']
+    config.assets.precompile += ['bottom_section.js']
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
