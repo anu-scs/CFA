0ae88f556d534047c0a56ebe3c422b3fbb9148ea
kapil <kchoudhary006@gmail.com>
2013-06-21 01:18:55 +0530

    campaign index to home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
   def index
+    @campaigns = Campaign.includes(:campaign_stat).active.limit 10
+    @pending_campaigns = Campaign.includes(:campaign_stat).pending.limit 10
+    
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 end
