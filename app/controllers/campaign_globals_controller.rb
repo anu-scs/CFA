888c9c19044b7431448a387deb8478d1a4eb03e5
kapil <kchoudhary006@gmail.com>
2013-06-23 04:41:29 +0530

    changes constant var

--- app/controllers/campaign_globals_controller.rb
+++ app/controllers/campaign_globals_controller.rb
@@ -1,23 +1,23 @@
 class CampaignGlobalsController < ApplicationController
 
   def get_total_calls_forever
-    total_calls_forever = CampaignGlobal.where(stat_code: "Donations_remaining").first rescue 0
-    render :text => total_calls_forever || 0
+    total_calls_forever = CampaignGlobal.where(stat_code: CampaignStat::TOTAL_CALLS_MADE_COUNT).first rescue 0
+    render :text => total_calls_forever.try(:value_life) || 0
   end
 
   def get_total_calls_today
-    total_calls_today = CampaignGlobal.where(stat_code: "Donations_remaining").first rescue 0
-    render :text => total_calls_today || 0
+    total_calls_today = CampaignGlobal.where(stat_code: CampaignStat::TOTAL_CALLS_MADE_COUNT).first rescue 0
+    render :text => total_calls_today.try(:value_today) || 0
   end
 
   def get_donation_remaining
-    donation_remaining = CampaignGlobal.where(stat_code: "Donations_remaining").first rescue 0
-    render :text => donation_remaining || 0
+    donation_remaining = CampaignGlobal.where(stat_code: CampaignStat::DONATIONS_REMAINING).first rescue 0
+    render :text => donation_remaining.try(:value_life) || 0
   end
 
   def get_calls_in_progress
-    calls_in_progress = CampaignGlobal.where(stat_code: "Calls_in_progress").first rescue 0
-    render :text => calls_in_progress || 0
+    calls_in_progress = CampaignGlobal.where(stat_code: CampaignStat::CALLS_IN_PROGRESS).first rescue 0
+    render :text => calls_in_progress.try(:value_life) || 0
   end
 
 end
